df451714d1250f0ddd2b86681ed9fd88ba85d557
Gabriel Mendes <gm.santos9423@gmail.com>
2026-03-28 23:30:59 -0300

    Fix output messages and formatting in T04_08.java
    Corrected spelling mistakes in output messages and ensured consistent formatting.

--- UC-Algoritmos e Programação/Prof_Calveti/T04/T04_08.java
+++ UC-Algoritmos e Programação/Prof_Calveti/T04/T04_08.java
@@ -23,19 +23,19 @@ public class T04_08 {
        }
       else if(n2 <= n1 && n1 <= n3)
       {
-        System.out.println("Segue os numeors em ordem crescente: " + n2 + ", " + n1 + ", " + n3);
+        System.out.println("Segue os numeros em ordem crescente: " + n2 + ", " + n1 + ", " + n3);
       }
       else if(n2 <= n3 && n3 <= n1)
       {
-        System.out.println("Segue os numeors em ordem crescente: " + n2 + ", " + n3 + ", " + n1);
+        System.out.println("Segue os numeros em ordem crescente: " + n2 + ", " + n3 + ", " + n1);
       }
       else if(n3 <= n2 && n2 <= n1)
       {
-        System.out.println("Segue os numeros em oredem crescente: " + n3 + ", " + n2 + ", " + n1);
+        System.out.println("Segue os numeros em ordem crescente: " + n3 + ", " + n2 + ", " + n1);
       }
       else if(n3 <= n1 && n1 <= n2)
       {
-        System.out.println("Segue os numeors em ordem crescente: " + n3 + ", " + n1 + ", " + n2);
+        System.out.println("Segue os numeros em ordem crescente: " + n3 + ", " + n1 + ", " + n2);
       }
 
         scanner.close();
